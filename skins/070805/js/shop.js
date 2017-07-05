45ecdb526970e5d6597b8ad561577e585f4af4a5
BJRambo <qw5414@naver.com>
2017-07-05 13:02:48 +0900

    radio 를 채크한 것만 가격변화실행

--- skins/070805/js/shop.js
+++ skins/070805/js/shop.js
@@ -1,6 +1,6 @@
 jQuery(function($) {
 	$('.selected_days').click(function() {
-		var $opt = $(this).children('input');
+		var $opt = $(this).children('input:checked');
 		var icon_srl = $opt.attr('data-icon-srl');
 		var day_price = $opt.attr('data-day-price');
 
@@ -12,9 +12,10 @@ jQuery(function($) {
 	});
 
 	$('.g_selected_days').click(function () {
-		var $opt = $(this).children('input');
+		var $opt = $(this).children('input:checked');
 		var icon_srl = $opt.attr('data-icon-srl');
 		var day_price = $opt.attr('data-day-price');
+
 		$.exec_json('iconshop.getPricePoint', {'icon_srl': icon_srl, 'day_price': day_price}, function (ret_obj) {
 			jQuery('#g_icon_price_' + icon_srl).html(ret_obj.icon_price + 'P');
 			jQuery('#g_day_price_' + icon_srl).html(day_price + 'P');
